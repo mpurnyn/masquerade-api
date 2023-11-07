1fc98cb0c09f4a9f3399f2cc08004e660f92df9d
mpurnyn <mpurnyn@gmail.com>
2023-11-07 12:01:12 -0600

    register models

--- django/django_site/facemasq/admin.py
+++ django/django_site/facemasq/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Mask, Photo, Video
+admin.site.register(Mask)
+admin.site.register(Photo)
+admin.site.register(Video)
