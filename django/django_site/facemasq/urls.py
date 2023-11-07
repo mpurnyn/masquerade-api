3a9264ce95b9ffa017ade5ba8c7116fcc8cc02e2
mpurnyn <mpurnyn@gmail.com>
2023-11-07 12:18:50 -0600

    create stubs for views and url paths

--- django/django_site/facemasq/urls.py
+++ django/django_site/facemasq/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "facemasq"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("masks/create", views.create_mask, name="create_mask"),
+    path("masks/delete", views.delete_mask, name="delete_mask"),
+    path("masks/view", views.view_masks, name="view_masks"),
+    path("photo/create", views.create_photo, name="create_photo"),
+    path("video/create", views.create_video, name="create_video"),
 ]
--- django/django_site/facemasq/views.py
+++ django/django_site/facemasq/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from .models import Mask, Photo, Video
 
 def index(request):
     return HttpResponse("Hello, world. You're at the facemasq index.")
+
+def create_mask(request, prompt):
+    return HttpResponse("Created a FaceMasq.")
+
+def delete_mask(request, mask_ID):
+    return HttpResponse("Deleted the FaceMasq.")
+
+def view_masks(request):
+    return Mask.objects.filter(user_ID=request.user)
+
+def create_photo(request):
+    return HttpResponse("Created a FaceMasq Photo.")
+
+def create_video(request):
+    return HttpResponse("Created a FaceMasq Video.")
