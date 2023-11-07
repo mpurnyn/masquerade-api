5a9ca2e1043a4ba38211f37eef42d96270a52e48
mpurnyn <mpurnyn@gmail.com>
2023-11-07 12:00:07 -0600

    defined models for maskerade app

--- django/django_site/facemasq/models.py
+++ django/django_site/facemasq/models.py
@@ -1,3 +1,33 @@
+# Create your models here
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Mask(models.Model):
+    mask_ID = models.primaryKey(primary_key=True, editable=False)
+    user_ID = models.foreignKey(User, required=True)
+    mask_location = models.URLField(max_length=200, required=True)
+    mask_prompt = models.CharField(max_length=200, required=True)
+    mask_seed = models.IntegerField(default=0, required=True)
+    mask_creation_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.cloudstorageID
+    
+class Photo(models.Model):
+    photo_ID = models.PrimaryKey(primary_key=True, editable=False)
+    user_ID = models.ForeignKey(User, required=True)
+    mask_ID = models.ForeignKey(Mask, required=True)
+    src_photo_location = models.URLField(max_length=200, required=True)
+    photo_location = models.URLField(max_length=200, required=True)
+    photo_creation_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.cloudstorageID
+    
+class Video(models.Model):
+    video_ID = models.PrimaryKey(primary_key=True, editable=False)
+    user_ID = models.ForeignKey(User, required=True)
+    mask_ID = models.ForeignKey(Mask, required=True)
+    src_video_location = models.URLField(max_length=200, required=True)
+    video_location = models.URLField(max_length=200, required=True)
+    video_creation_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.cloudstorageID
