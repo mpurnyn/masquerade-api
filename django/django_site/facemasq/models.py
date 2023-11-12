7abe6705d3450a8e4c82d1462ba28eaf3f467054
mpurnyn <mpurnyn@gmail.com>
2023-11-12 00:40:32 -0500

    updated fk user id per documentation

--- django/django_site/facemasq/models.py
+++ django/django_site/facemasq/models.py
@@ -1,10 +1,10 @@
 # Create your models here
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Mask(models.Model):
     mask_ID = models.primaryKey(primary_key=True, editable=False)
-    user_ID = models.foreignKey(User, required=True)
+    user_ID = models.foreignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, required=True)
     mask_location = models.URLField(max_length=200, required=True)
     mask_prompt = models.CharField(max_length=200, required=True)
     mask_seed = models.IntegerField(default=0, required=True)
@@ -14,7 +14,7 @@ class Mask(models.Model):
     
 class Photo(models.Model):
     photo_ID = models.PrimaryKey(primary_key=True, editable=False)
-    user_ID = models.ForeignKey(User, required=True)
+    user_ID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, required=True)
     mask_ID = models.ForeignKey(Mask, required=True)
     src_photo_location = models.URLField(max_length=200, required=True)
     photo_location = models.URLField(max_length=200, required=True)
@@ -24,7 +24,7 @@ class Photo(models.Model):
     
 class Video(models.Model):
     video_ID = models.PrimaryKey(primary_key=True, editable=False)
-    user_ID = models.ForeignKey(User, required=True)
+    user_ID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, required=True)
     mask_ID = models.ForeignKey(Mask, required=True)
     src_video_location = models.URLField(max_length=200, required=True)
     video_location = models.URLField(max_length=200, required=True)
